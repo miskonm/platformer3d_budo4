5b20f4ce3469f0d2332abe7c3b41e140479ecefd
Nikita Misko <miskonm@gmail.com>
2022-10-13 18:14:19 +0300

    Init

---
+++ Assets/Scripts/PlayerRotator.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class PlayerRotator : MonoBehaviour
+{
+    [SerializeField] private float _speed = 0.1f;
+    
+    private Vector3 _previousMousePosition;
+
+    private void Start()
+    {
+        _previousMousePosition = Input.mousePosition;
+    }
+
+    private void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 delta = _previousMousePosition - mousePosition;
+        float rotationDelta = delta.x;
+        transform.Rotate(transform.up, rotationDelta * _speed * Time.deltaTime);
+        _previousMousePosition = mousePosition;
+    }
+}
